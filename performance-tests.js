3d7a0ad09b3186e46fa88ee743502b5dc39b647d
“pourdanisj” <pourdanisj@vmware.com>
2023-04-19 13:24:28 +0300

    simple run

--- performance-tests.js
+++ performance-tests.js
@@ -2,8 +2,8 @@ import { check, sleep } from "k6";
 import http from "k6/http";
 
 export const options = {
-  vus: 10, // 10 virtual users
-  duration: "30s", // Load duration.
+  vus: 1, // 10 virtual users
+  duration: "5s", // Load duration.
   thresholds: {
     http_req_failed: ["rate<0.01"], // http errors should be less than 1%
     http_req_duration: ["p(95)<200"], // 95% of requests should be below 200ms
